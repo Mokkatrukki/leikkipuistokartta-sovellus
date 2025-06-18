2cda0191d3aa981c80216bb4d07a78da3b4f02ea
Leo Vainio <leo.vainio@solita.fi>
2025-06-18 22:15:29 +0300

    chore: Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/leikkipuistokartta-sovellus/', // Set the base path for GitHub Pages
   plugins: [
     tailwindcss(),
     react()
